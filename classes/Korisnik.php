2f9bd5c1292a089fa512b2c2ae75fa73357d5c2a
GaGiiiii <dragoslav.gagi8@yahoo.com>
2020-04-07 12:15:33 +0200

    Fixed Korisnik constructor

--- classes/Korisnik.php
+++ classes/Korisnik.php
@@ -19,8 +19,6 @@
             $this->email = $email;
             $this->sifra = $sifra;
             $this->connection = Database::getInstance()->getConnection();
-            $instance = Database::getInstance();
-            $this->connection = $instance->getConnection();
         }
 
         public function registruj(){
